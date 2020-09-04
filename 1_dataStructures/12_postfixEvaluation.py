4408a6c024239c185bdcff0ac378b0cb656b96ff
snehil <44499005+snehilk1312@users.noreply.github.com>
2020-09-04 19:02:09 +0530

    Added prefix and postfix evaluation using Stack

---
+++ 1_dataStructures/12_postfixEvaluation.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class Stack:
+    def __init__(self, top=None):
+        self.top = top
+        self.count = 0
+    def push(self, value):
+        newNode = Node(value)
+        temp = self.top
+        self.top = newNode
+        self.top.next = temp
+        self.count += 1
+
+    def pop_(self):
+        temp = self.top
+        self.top = temp.next
+        temp.next = None
+        self.count -= 1
+        return temp.data
+
+    def isEmpty(self):
+        return self.count==0
+
+
+mystack = Stack()
+expression = input("Enter the postfix expression with space as delimiter: ")
+expression = expression.split(' ')
+for i in range(len(expression)):
+    try:
+        temp = float(expression[i])
+        mystack.push(expression[i])
+    except ValueError:
+        operator = expression[i]
+        operand2 = mystack.pop_()
+        operand1 = mystack.pop_()
+        result = eval(f'{operand1}{operator}{operand2}')
+        mystack.push(result)
+    except:
+        print("Enter valid Postfix expression")
+        exit()
+print(mystack.pop_())
